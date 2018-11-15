bae50570164b81940b2016ac93b2b2cbd141b00f
shaynaor <44779960+shaynaor@users.noreply.github.com>
2018-11-15 05:14:38 +0200

    delete old test

--- MyMath/src/myMath/Test_new.java
+++ MyMath/src/myMath/Test_new.java
@@ -4,12 +4,7 @@ package myMath;
 public class Test_new {
 
 	public static void main(String[] args) {
-		Polynom m1 = new Polynom("0.2x^4-1.5x^3+3.0x^2-x-5"); //bug 2x^2+6-3x^3
-	
-	
-	  
-		
-		
+		Polynom m1 = new Polynom("0.2x^4-1.5x^3+3.0x^2-x-5");
 		PlotPolynom frame = new PlotPolynom(m1,-2,6);
 	
 		frame.setVisible(true);
